7fa8607447ebc706259a82bb10f38012ff55bded
Alexandre Arcanjo de Queiroz <alexandre.queiroz@live.com>
2021-06-01 12:52:49 -0300

    Clean files script

--- clean_files.sh
+++ clean_files.sh
@@ -3,7 +3,9 @@ sudo journalctl --vacuum-time=1d
 
 sudo apt-get autoclean -y
 
-sudo apt-get autoremove -y
+sudo apt-get clean -y
+
+sudo apt-get autoremove -y --purge
 
 if [ -x "$(command -v docker)" ]; then
 	docker system prune -a --volumes -f
