b9b93d7c127957e523cff90f272393ffd9f1ebd2
Alexandre Arcanjo de Queiroz <arcanjoaq@gmail.com>
2021-07-09 18:27:04 -0300

    Removing additional sh

--- install.sh
+++ install.sh
@@ -197,17 +197,6 @@ function install_golang() {
       sudo ln -sf /usr/local/${GO_VERSION} /usr/local/go && \
       rm ${GO_VERSION}.linux-amd64.tar.gz && \
       mkdir -p ${HOME}/golang
-
-      cat ${HOME}/.profile | grep 'export GOPATH=' > /dev/null 2>&1
-      [ $? -ne 0 ] && echo 'export GOPATH=${HOME}/golang' | tee -a ${HOME}/.profile
-      
-      cat ${HOME}/.profile | grep 'export PATH=${PATH}:/usr/local/go/bin' > /dev/null 2>&1
-      [ $? -ne 0 ] && echo 'export PATH=${PATH}:/usr/local/go/bin' | tee -a ${HOME}/.profile
-
-      cat ${HOME}/.profile | grep 'export GOBIN=' > /dev/null 2>&1
-      [ $? -ne 0 ] && echo 'export GOBIN=/usr/local/go/bin' | tee -a ${HOME}/.profile
-
-      source ${HOME}/.profile
   else
     echo "GoLang is already installed"
   fi
@@ -229,15 +218,6 @@ function install_antlr() {
   if ! [ -f "/usr/local/lib/antlr-4.9.2-complete.jar" ]; then
     cd /usr/local/lib
     sudo curl -O https://www.antlr.org/download/antlr-4.9.2-complete.jar
-
-    cat ${HOME}/.bash_aliases | grep 'alias antlr4=' > /dev/null 2>&1
-    [ $? -ne 0 ] && echo "alias antlr4='java -jar /usr/local/lib/antlr-4.9.2-complete.jar'" | tee -a ${HOME}/.bash_aliases
-
-    cat ${HOME}/.bash_aliases | grep 'alias grun=' > /dev/null 2>&1
-    [ $? -ne 0 ] && echo "alias grun='java org.antlr.v4.gui.TestRig'" | tee -a ${HOME}/.bash_aliases
-
-    cat ${HOME}/.profile | grep 'export CLASSPATH=".:/usr/local/lib/antlr-4.9.2-complete.jar:$CLASSPATH"' > /dev/null 2>&1
-    [ $? -ne 0 ] && echo 'export CLASSPATH=".:/usr/local/lib/antlr-4.9.2-complete.jar:$CLASSPATH"' | tee -a ${HOME}/.profile
   fi    
 }
 
